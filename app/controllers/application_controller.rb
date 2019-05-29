7e73eb10acae1024eed87aee8277b2fa0b811d5e
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-05-29 21:33:31 +0100

    Flash message for successful sign-ups and sew session for signed-up user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+  # Make current_user method available in views
+  helper_method :current_user
+
+  # Run this on every page before any action
+  before_action :current_user
+
+  def current_user
+    if session[:user_id].present?
+      @current_user = User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+
+    @current_user # Return something to use
+  end
+
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
   def create # This happens when user hits the submit button on the form
     @user = User.new(form_params)
     if @user.save
+      # Save session with user
+      session[:user_id] = @user.id
+      # Let user know they've signed-up sucessfully
+      flash[:success] = "Thanks for creating an account"
       redirect_to root_path
     else
       render "new"
