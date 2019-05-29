7d8ed23d8f028f5b0cc7bb37d158c7834694e1c3
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-05-29 21:08:41 +0100

    Create method for sign-up form. Saves user details to form_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
 
-  def new
+  def new # This is the form view
     @user = User.new
+  end
+
+  def create # This happens when user hits the submit button on the form
+    @user = User.new(form_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  def form_params
+    params.require(:user).permit(:name, :username,
+      :email, :password, :password_confirmation)
   end
 
 end
