b0765b2f877c71fe15eb24db46e10c47b6036da5
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-05-29 21:55:47 +0100

    Allow users to log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,13 @@
 class SessionsController < ApplicationController
+
+
+  def destroy
+    # Log-out by removing session completely with Rails' "reset_session" method
+    reset_session
+
+    flash[:success] = "You've logged out!"
+
+    redirect_to root_path
+  end
+
 end
