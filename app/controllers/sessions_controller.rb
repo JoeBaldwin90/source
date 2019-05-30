00c897c8fc0a9f6ce1ba19d2ae3e4b11a88ed02a
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-05-30 13:26:05 +0100

    Allow user log-in by assigning the user ID to the session ID

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,24 @@ class SessionsController < ApplicationController
     # not adding anything to a database
   end
 
+  def create
+    # Pull username and password from the form form data
+    @username = form_params[:username]
+    @password = form_params[:password]
+
+    # Check user's credentials
+    @user = User.find_by(username: @username).try(:authenticate, @password)
+
+    # If user exists, redirect to homepage.
+    if @user.present?
+      session[:user_id] = @user.id # Save this session to user id
+      flash[:success] = "Logged in!"
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
   def destroy
     # Log-out by removing session completely with Rails' "reset_session" method
     reset_session
@@ -12,6 +30,10 @@ class SessionsController < ApplicationController
     flash[:success] = "You've logged out!"
 
     redirect_to root_path
+  end
+
+  def form_params
+    params.require(:session).permit(:username, :password)
   end
 
 end
