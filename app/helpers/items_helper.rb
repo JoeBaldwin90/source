4a86ba791d11b8d729647d46ec9b9504289cdfc2
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-06-05 10:54:03 +0100

    Format categories custom helper

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,13 @@
 module ItemsHelper
+
+  def format_categories(categories)
+
+    links = categories.map do |c|
+      link_to c.title, category_path(c), class: "link"
+    end
+
+    links.to_sentence.html_safe
+
+  end
+
 end
