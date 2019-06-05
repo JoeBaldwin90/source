0efc600c32eac1e59bd151cd5fb5d7d385f6ab59
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-06-05 10:34:54 +0100

    Category, Item & CategoryItem associations

--- app/admin/items.rb
+++ app/admin/items.rb
@@ -1,15 +1,17 @@
 ActiveAdmin.register Item do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-permit_params :image, :title, :description
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+
+permit_params :image, :title, :description,
+  category_ids: [] # because category ids are an array
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :description
+      f.input :image
+      f.input :categories, as: :check_boxes,
+        collection: Category.order("title asc")
+      end
+    f.actions
+  end
 
 end
