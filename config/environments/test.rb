757672757fa8b94b6872052afb78e1c3b1584b49
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-06-06 23:37:25 +0100

    No master key needed in test mode

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.require_master_key = true
+  config.require_master_key = false
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
