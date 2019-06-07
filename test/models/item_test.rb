d56118d0f82f3f13d47636d1e5e2213ad3974953
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-06-07 14:04:04 +0100

    Basic testing

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "item has a title and image" do
+    @item = items(:one)
+
+    assert_equal @item.title.present?, true, "No title present"
+    assert_equal @item.image.present?, true, "No image present"
+    assert_equal @item.description.present?, true, "No description present"
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Uer should not save when all fields are blank' do
+    assert_equal User.new.save, false, 'User saved with blank fields'
+  end
+
+  test 'User should save if all required fields are full' do
+    @user = users(:joe)
+
+    assert_equal @user.save, true, 'User saved when some fields are empty'
+  end
+
+  test 'User should be able to save and subscribe on signup' do
+    @user = users(:joe)
+
+    assert_equal @user.save_and_subscribe, true, 'User could not save & subscribe with thier details'
+  end
+
+  test 'User should not save and subscribe with invalid subscription plan' do
+    @user = users(:joe)
+    @user.subscription_plan = 'fakeplan'
+
+    assert_equal @user.save_and_subscribe, false, 'User saved and subscribed with invalud sub plan'
+  end
+
+  test 'We should not charge user if their card is declined' do
+    @user = users(:milan)
+
+    assert_equal @user.save_and_subscribe, false, 'User was created and subscribed even though they used a declined card'
+    assert_equal @user.errors[:stripe_token], ['Your card was declined.'], "The card was declined, but didn't match expected error message"
+  end
+
+  test 'should report random error' do
+    # some_undefined_variable is not defined elsewhere in the test case
+    assert_raises(NameError) do
+      some_undefined_variable
+    end
+  end
 end
